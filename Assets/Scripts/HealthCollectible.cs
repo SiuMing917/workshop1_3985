2ccd3145fa8e9f4a119fe4352f050c1552eb4e88
SiuMing917 <23020776d@connect.polyu.hk>
2024-03-07 14:12:53 +0800

    final

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    public int healAmount;
-    // this will be called whenever other objects enter the trigger zone
+    public AudioClip collectedClip;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        // wrtie your healing process here
-        if (other.tag == "Player") // check if the incoming object is player
-        {
-            RubyController controller = other.GetComponent<RubyController>();
+        RubyController controller = other.GetComponent<RubyController>();
 
+        if (controller != null)
+        {
             if (controller.health < controller.maxHealth)
             {
-                print("Player HP is now " + controller.health);
-                controller.ChangeHealth(healAmount); //heal the player for heal amount
-                //controller.HP += healAmount; //  this is wrong
-                Destroy(this.gameObject);
-            }   // "this" means myself ( the script)
-                // "gameObject" means the whole gameObject
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+
+                controller.PlaySound(collectedClip);
+            }
         }
+
     }
 }
